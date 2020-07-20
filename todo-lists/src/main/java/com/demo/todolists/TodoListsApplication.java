fcd0461715d0b48ac6d98bd893f915478098aaf6
Mehmet Ali <mali.caferx@gmail.com>
2020-07-20 16:10:07 +0900

    first commit

---
+++ todo-lists/src/main/java/com/demo/todolists/TodoListsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.todolists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListsApplication.class, args);
+	}
+
+}
---
+++ todo-lists/src/test/java/com/demo/todolists/TodoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.todolists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
