75f32f50c4d09f37db8970b9ef1d42d866d82d11
Mehmet Ali <mali.caferx@gmail.com>
2020-07-22 10:43:18 +0900

    added pom.xmls for build frontend and backend same time

---
+++ frontend/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ...
+    // Change build paths to make them Maven compatible
+    // see https://cli.vuejs.org/config/
+    outputDir;: 'target/dist',
+    assetsDir;: 'static';
+  }
