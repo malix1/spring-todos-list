3336cb06bd206fa06f5a77bcfbef5db1a17a1a78
Mehmet Ali <mali.caferx@gmail.com>
2020-07-27 15:20:57 +0900

    added todos page

--- frontend/src/routes/userRoutes.js
+++ frontend/src/routes/userRoutes.js
@@ -1,5 +1,7 @@
 import MainPage from "../views/Main";
+import TodosPage from "../views/Todos";
 
 export const userRoutes = [
   { path: "/", component: MainPage },
+  { path: "/todos", component: TodosPage },
 ];
---
+++ frontend/src/views/Todos/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      Todos Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<style scoped>
+</style>
