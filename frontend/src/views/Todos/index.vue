cbd4a6dc5648ec8ec2ef4745319f7824cf7d4f4c
Mehmet Ali <mali.caferx@gmail.com>
2020-07-27 16:10:55 +0900

    added default value for todos

--- frontend/src/views/Todos/index.vue
+++ frontend/src/views/Todos/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-      Todos Page
+    Todos Page
+    <div v-for="todo in todos" :key="todo.id"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Todos"
+  name: "Todos",
+  data: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: "Write kanji",
+          isDone: false
+        },
+        {
+          id: 2,
+          name: "Study grammar",
+          isDone: false
+        }
+      ]
+    };
+  }
 };
 </script>
 
